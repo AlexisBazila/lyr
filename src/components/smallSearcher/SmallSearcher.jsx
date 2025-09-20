f675de7ff2857fc224ce648a2ac3a7e1c7d05931
Alexis Bazila <alexisbazila@proton.me>
2025-09-20 18:20:49 -0300

    AdvancedSelect

--- src/components/smallSearcher/SmallSearcher.jsx
+++ src/components/smallSearcher/SmallSearcher.jsx
@@ -7,12 +7,14 @@ function SmallSearcher() {
     <div className="smallSearcher">
       <div className="searcherContent">
         <div className="title">
-          <h3>Encontrá lo que estas buscando</h3>
+          <h3 className="text-3xl text-black">
+            Encontrá lo que estas buscando
+          </h3>
         </div>
         <form className="formSearch" action="">
           <div class="relative mb-6">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none text-2xl">
-              <LuMapPin />
+              <LuMapPin className="text-black" />
             </div>
             <input
               type="text"
@@ -23,24 +25,17 @@ function SmallSearcher() {
           </div>
 
           {/* SELECT */}
-          <div class="max-w-sm">
-            <select
-              data-select='{
-    "placeholder": "<span class=\"inline-flex items-center\"><span class=\"icon-[tabler--filter] shrink-0 size-4 me-2\"></span> Filter</span>",
-    "toggleTag": "<button type=\"button\" aria-expanded=\"false\"></button>",
-    "toggleClasses": "advance-select-toggle select-disabled:pointer-events-none select-disabled:opacity-40",
-    "dropdownClasses": "advance-select-menu",
-    "optionClasses": "advance-select-option selected:select-active",
-    "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"icon-[tabler--check] shrink-0 size-4 text-primary hidden selected:block \"></span></div>",
-    "extraMarkup": "<span class=\"icon-[tabler--caret-up-down] shrink-0 size-4 text-base-content absolute top-1/2 end-3 -translate-y-1/2 \"></span>"
-  }'
-              class="hidden"
-            >
-              <option value="">Choose</option>
-              <option value="name">Full Name</option>
-              <option value="email">Email Address</option>
-              <option value="description">Project Description</option>
-              <option value="user_id">User Identification Number</option>
+          <div class="w-96 select">
+            <span class="icon-[tabler--movie] text-base-content/80 my-auto size-5 shrink-0"></span>
+            <label class="sr-only" for="favorite-simpson">
+              Pick your favorite Movie
+            </label>
+            <select id="favorite-simpson">
+              <option>The Godfather</option>
+              <option>The Shawshank Redemption</option>
+              <option>Pulp Fiction</option>
+              <option>The Dark Knight</option>
+              <option>Schindler's List</option>
             </select>
           </div>
         </form>
