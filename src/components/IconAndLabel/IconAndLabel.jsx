32f82aea7cce8464c5e29bdcea48e1f8923d5432
Alexis Bazila <alexisbazila@proton.me>
2025-12-22 14:16:00 -0300

    correccion de tipeo IconAndLabel

--- src/components/IconAndLabel/IconAndLabel.jsx
+++ src/components/IconAndLabel/IconAndLabel.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./iconAndLabel.css";
+import "./IconAndLabel.css";
 import { FaHouse } from "react-icons/fa6";
 function IconAndLabel({ icon, label }) {
   return (
